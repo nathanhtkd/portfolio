5807108692555986c694098d1824abf79699fd00
Nathan Hartojo <nathanhartojo@gmail.com>
2023-12-20 17:42:32 -0600

    add tailwind, remove unneeded files, set up basic structure of portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from "./logo.svg";
+// src/App.js
 import "./App.css";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
-function App() {
+export default function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Test Edit.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <main>
+            <Navbar />
+            <About />
+            <Projects />
+            <Skills />
+            <Contact />
+        </main>
     );
 }
-
-export default App;
