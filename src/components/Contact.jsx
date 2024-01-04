a537023d01b52e34659f83f81361096cc4760f94
Nathan Hartojo <nathanhartojo@gmail.com>
2024-01-03 20:09:10 -0600

    fix form submissions

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -67,6 +67,7 @@ export default function Contact() {
                 </div>
                 <form
                     netlify
+                    data-netlify="true"
                     name="contact"
                     onSubmit={handleSubmit}
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
@@ -125,6 +126,8 @@ export default function Contact() {
                         className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                         Submit
                     </button>
+                    <input type="hidden" name="form-name" value="contact" />
+                    <input name="bot-field" className="hidden" />
                 </form>
             </div>
         </section>
