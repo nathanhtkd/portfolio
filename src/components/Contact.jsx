e8ecb8cffa287a8ed6d2fc5c46ca9dab99d3d163
Nathan Hartojo <nathanhartojo@gmail.com>
2024-01-03 20:17:40 -0600

    fix contact form on netlify

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -67,8 +67,8 @@ export default function Contact() {
                 </div>
                 <form
                     netlify
-                    data-netlify="true"
                     name="contact"
+                    method="post"
                     onSubmit={handleSubmit}
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
@@ -127,7 +127,6 @@ export default function Contact() {
                         Submit
                     </button>
                     <input type="hidden" name="form-name" value="contact" />
-                    <input name="bot-field" className="hidden" />
                 </form>
             </div>
         </section>
