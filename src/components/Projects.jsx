c29826cda9fd39155360022f95ec8df186c30672
nathanhtkd <nathanhartojo@gmail.com>
2024-01-05 02:28:43 -0600

    add images to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,13 @@
 import { useState } from 'react';
 import { CodeBracketIcon } from '@heroicons/react/24/solid';
 import { projects } from '../data';
-import w5ac from '../images/tamu-zachry.jpg';
-const images = [w5ac, w5ac, w5ac];
+import hifive from '../images/hifive.png';
+import w5ac from '../images/tamuw5aclogo.png';
+import roboteyes from '../images/roboteyes.jpg';
+import marky from '../images/markylogo.jpeg';
+import nftbanner from '../images/nftbanner.jpg';
+import zachry from '../images/tamu-zachry.jpg';
+const images = [w5ac, hifive, roboteyes, marky, nftbanner, zachry];
 
 /**
  * Gallery of projects that I've worked on. Includes images, descriptions, and links.
@@ -16,14 +21,11 @@ export default function Projects() {
     return (
         <section id="projects" className="text-gray-400 bg-gray-900 body-font">
             <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-                <div className="flex flex-col w-full mb-20">
+                <div className="flex flex-col w-full mb-12">
                     <CodeBracketIcon className="mx-auto inline-block w-10 mb-4" />
                     <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
                         Stuff I've Built
                     </h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                        In development!
-                    </p>
                 </div>
                 <div className="grid md:grid-cols-2 gap-6">
                     {projects.map((project, index) => (
